0249e3afe02386784577b1240303107a6b48801c
yizhong187 <yizhong187@gmail.com>
2024-05-09 17:06:26 +0800

    removed updated_at field for table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,9 +3,8 @@
 CREATE TABLE users (
   id UUID PRIMARY KEY,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
-  name TEXT NOT NULL
-  valid BOOLEAN DEFAULT FALSE
+  name TEXT NOT NULL,
+  valid BOOLEAN DEFAULT FALSE NOT NULL
 );
 
 -- +goose Down
