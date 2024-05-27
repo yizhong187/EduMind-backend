049a19edb41094c1ac012f428eb3dda0a585155a
yizhong187 <yizhong187@gmail.com>
2024-05-27 11:44:19 +0800

    added getUser handler

--- handlers/handler_user.go
+++ handlers/handler_user.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 
@@ -10,12 +11,14 @@ import (
 	"github.com/yizhong187/EduMind-backend/internal/database"
 	"github.com/yizhong187/EduMind-backend/internal/domain"
 	"github.com/yizhong187/EduMind-backend/internal/util"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func (apiCfg *ApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	// local struct to hold expected data from the request body
 	type parameters struct {
-		Name string
+		Name     string
+		Password string
 	}
 
 	params := parameters{}
@@ -26,16 +29,52 @@ func (apiCfg *ApiConfig) HandlerCreateUser(w http.ResponseWriter, r *http.Reques
 	}
 	defer r.Body.Close()
 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(params.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		Name:      params.Name,
-		Valid:     true,
+		ID:             uuid.New(),
+		CreatedAt:      time.Now().UTC(),
+		Name:           params.Name,
+		Valid:          true,
+		HashedPassword: string(hashedPassword),
 	})
 
 	if err != nil {
 		fmt.Println(err)
 		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return
+	}
+
+	util.RespondWithJSON(w, http.StatusOK, domain.DatabaseUserToUser(user))
+}
+
+func (apiCfg *ApiConfig) HandlerGetUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		ID string
+	}
+
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+	defer r.Body.Close()
+
+	parsedUUID, err := uuid.Parse(params.ID)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Invalid UUID")
+		return
+	}
+
+	user, err := apiCfg.DB.GetUser(r.Context(), parsedUUID)
+
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Could not get user info")
 		return
 	}
 
