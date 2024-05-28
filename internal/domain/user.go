34f1fd673312ffa124ecf604040443b537d0622f
yizhong187 <yizhong187@gmail.com>
2024-05-29 00:06:29 +0800

    updated for new schema

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -1,26 +1,20 @@
 package domain
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 	"github.com/yizhong187/EduMind-backend/internal/database"
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id"`
-	Username  string    `json:"username"`
-	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name"`
-	Valid     bool      `json:"valid"`
+	ID       uuid.UUID `json:"user_id"`
+	Username string    `json:"username"`
+	Type     string    `json:"user_type"`
 }
 
 func DatabaseUserToUser(user database.User) User {
 	return User{
-		ID:        user.ID,
-		Username:  user.Username,
-		CreatedAt: user.CreatedAt,
-		Name:      user.Name,
-		Valid:     user.Valid,
+		ID:       user.UserID,
+		Username: user.Username,
+		Type:     user.UserType,
 	}
 }
