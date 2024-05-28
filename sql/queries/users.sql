8ce513ceffe269f1f4497a60ff4f7bb9288fbd66
yizhong187 <yizhong187@gmail.com>
2024-05-28 14:22:36 +0800

    added 2 new sql queries

--- sql/queries/users.sql
+++ sql/queries/users.sql
@@ -1,7 +1,13 @@
 -- name: CreateUser :one
-INSERT INTO users (id, created_at, name, valid, hashed_password)
-VALUES ($1, $2, $3, $4, $5)
+INSERT INTO users (id, username, created_at, name, valid, hashed_password)
+VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
--- name: GetUser :one
+-- name: GetUserById :one
 SELECT * FROM users WHERE id = $1;
+
+-- name: GetUserByUsername :one
+SELECT * FROM users WHERE username = $1;
+
+-- name: GetHash :one
+SELECT hashed_password FROM users WHERE username = $1;
