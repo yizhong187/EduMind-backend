a2bab16b390d20be4d6b816fd46a1f8bcb74315b
yizhong187 <yizhong187@gmail.com>
2024-05-31 13:56:57 +0800

    added TODO

--- routers/student_chat_router.go
+++ routers/student_chat_router.go
@@ -11,8 +11,10 @@ func StudentChatRouter(apiCfg *handlers.ApiConfig) *chi.Mux {
 	r.Get("/", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerStudentGetAllChats))
 	r.Post("/", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerStartNewChat))
 
-	r.Get("/{chatID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerChat))
-	r.Post("/{chatID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerSendMessage))
+	// TODO: Add an additional middleware to check if the the studentID from the auth matches the chat's
+	//       student_id
+	r.Get("/{chatID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerStudentGetAllMessages))
+	r.Post("/{chatID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerStudentNewMessage))
 
 	// r.Delete("/{chatID}/{threadID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerDeleteMessage))
 	// r.Put("/{chatID}/{threadID}", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerEditMessage))
