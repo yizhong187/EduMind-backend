2208e02dd92148909ecbd04bddd5cdbdb4409dea
yizhong187 <yizhong187@gmail.com>
2024-06-02 00:15:10 +0800

    edited method signature to match chi's router Use method

--- middlewares/middleware_config.go
+++ middlewares/middleware_config.go
@@ -4,18 +4,13 @@ import (
 	"context"
 	"net/http"
 
+	"github.com/yizhong187/EduMind-backend/contextKeys"
 	"github.com/yizhong187/EduMind-backend/internal/config"
 )
 
-type contextKey string
-
-const (
-	ConfigKey contextKey = "config"
-)
-
-func MiddlewareApiConfig(next http.HandlerFunc, apiCfg *config.ApiConfig) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), ConfigKey, apiCfg)
-		next(w, r.WithContext(ctx))
-	}
+func MiddlewareApiConfig(next http.Handler, apiCfg *config.ApiConfig) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), contextKeys.ConfigKey, apiCfg)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
