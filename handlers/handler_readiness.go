1a6af5ca3d348d027dd6986c1f6532bac5f0791c
yizhong187 <yizhong187@gmail.com>
2024-06-02 00:12:03 +0800

    added catching config not found error

--- handlers/handler_readiness.go
+++ handlers/handler_readiness.go
@@ -3,14 +3,18 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/yizhong187/EduMind-backend/contextKeys"
 	"github.com/yizhong187/EduMind-backend/internal/config"
 	"github.com/yizhong187/EduMind-backend/internal/util"
-	"github.com/yizhong187/EduMind-backend/middlewares"
 )
 
 func HandlerReadiness(w http.ResponseWriter, r *http.Request) {
 
-	apiCfg := r.Context().Value(middlewares.ConfigKey).(config.ApiConfig)
+	apiCfg, ok := r.Context().Value(contextKeys.ConfigKey).(*config.ApiConfig)
+	if !ok || apiCfg == nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Configuration not found")
+		return
+	}
 
-	util.RespondWithJSON(w, 200, apiCfg)
+	util.RespondWithJSON(w, http.StatusOK, struct{}{})
 }
