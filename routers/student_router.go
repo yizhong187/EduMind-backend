cd397d26e6318c9f01bd74b2c4bf24868607d9d5
yizhong187 <yizhong187@gmail.com>
2024-06-02 00:14:13 +0800

    minor edits to reflect change in project structure

--- routers/student_router.go
+++ routers/student_router.go
@@ -3,19 +3,21 @@ package routers
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/yizhong187/EduMind-backend/handlers"
+	"github.com/yizhong187/EduMind-backend/internal/config"
+	"github.com/yizhong187/EduMind-backend/middlewares"
 )
 
-func StudentRouter(apiCfg *handlers.ApiConfig) *chi.Mux {
+func StudentRouter(apiCfg *config.ApiConfig) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/healthz", handlers.HandlerReadiness)
 		r.Get("/err", handlers.HandlerError)
 
-		r.Post("/register", apiCfg.HandlerStudentRegistration)
+		r.Post("/register", (handlers.HandlerStudentRegistration))
 
-		r.Get("/profile", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerGetStudentProfile))
-		r.Put("/profile", apiCfg.MiddlewareStudentAuth(apiCfg.HandlerUpdateStudentProfile))
+		r.Get("/profile", middlewares.MiddlewareStudentAuth(handlers.HandlerGetStudentProfile, apiCfg))
+		r.Put("/profile", middlewares.MiddlewareStudentAuth(handlers.HandlerUpdateStudentProfile, apiCfg))
 
 		r.Mount("/{studentID}", StudentChatRouter(apiCfg))
 
--- routers/tutor_router.go
+++ routers/tutor_router.go
@@ -3,17 +3,19 @@ package routers
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/yizhong187/EduMind-backend/handlers"
+	"github.com/yizhong187/EduMind-backend/internal/config"
+	"github.com/yizhong187/EduMind-backend/middlewares"
 )
 
-func TutorRouter(apiCfg *handlers.ApiConfig) *chi.Mux {
+func TutorRouter(apiCfg *config.ApiConfig) *chi.Mux {
 	r := chi.NewRouter()
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/healthz", handlers.HandlerReadiness)
 		r.Get("/err", handlers.HandlerError)
 
-		r.Get("/profile", apiCfg.MiddlewareTutorAuth(apiCfg.HandlerGetTutorProfile))
-		r.Put("/profile", apiCfg.MiddlewareTutorAuth(apiCfg.HandlerUpdateTutorProfile))
+		r.Get("/profile", middlewares.MiddlewareTutorAuth(handlers.HandlerGetTutorProfile, apiCfg))
+		r.Put("/profile", middlewares.MiddlewareTutorAuth(handlers.HandlerUpdateTutorProfile, apiCfg))
 
 	})
 
