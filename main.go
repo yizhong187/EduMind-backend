cefd6c16e1db7893924e7a745260ed98696a262d
yizhong187 <yizhong187@gmail.com>
2024-06-05 16:06:37 +0800

    added logger middleware and updated port for deployment

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 	"github.com/yizhong187/EduMind-backend/handlers"
@@ -24,15 +26,16 @@ func main() {
 	// note that godotenv needs to be installed.
 	err := godotenv.Load(".env")
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal("Error loading .env file")
 	}
 
 	// retrieving the environment variables, if not set a fatal error will be logged and programme will be terminated.
 	portString := os.Getenv("PORT")
 	if portString == "" {
-		log.Fatal("PORT is not found in the environment")
+		portString = "8080" // Default port if not set
+		log.Printf("No PORT environment variable detected. Defaulting to %s\n", portString)
 	}
-
 	dbURL := os.Getenv("DB_URL")
 	if dbURL == "" {
 		log.Fatal("DB_URL is not found in the environment")
@@ -47,6 +50,7 @@ func main() {
 	// thrown during a query and not during the the opening of the connection.
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal("Unable to connect to database:", err)
 	}
 
@@ -74,6 +78,7 @@ func main() {
 	}))
 
 	v1Router := chi.NewRouter()
+	v1Router.Use(middleware.Logger)
 	v1Router.Use(func(next http.Handler) http.Handler {
 		return middlewares.MiddlewareApiConfig(next, &apiCfg)
 	})
@@ -99,6 +104,7 @@ func main() {
 
 	err = srv.ListenAndServe()
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 }
