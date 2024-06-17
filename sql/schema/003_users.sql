86aadf4d27c971e138e1c8c88cfa072d47916425
yizhong187 <yizhong187@gmail.com>
2024-06-17 15:16:04 +0800

    removed goose migrations

--- sql/schema/003_users.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-CREATE TABLE users (
-  user_id UUID PRIMARY KEY,
-  username TEXT UNIQUE NOT NULL,
-  user_type VARCHAR(50) CHECK (user_type IN ('tutor', 'student', 'admin')) NOT NULL
-);
-
--- +goose Down
-DROP TABLE users;
