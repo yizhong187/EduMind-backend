c631eb646bf16960b783abf9ce4146f9c1c80e4f
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:12:14 +0800

    added student and tutor specific logins

--- handlers/handler_login.go
+++ handlers/handler_login.go
@@ -13,7 +13,7 @@ import (
 	"github.com/yizhong187/EduMind-backend/internal/util"
 )
 
-// HandlerLogin handles the request to login to an existing user. A cookie containing the JWT will be returned.
+// TO BE DELETED! REPLACED BY STUDENT AND TUTOR SPECIFIC LOGINS
 func HandlerLogin(w http.ResponseWriter, r *http.Request) {
 
 	apiCfg := r.Context().Value(contextKeys.ConfigKey).(*config.ApiConfig)
@@ -102,6 +102,178 @@ func HandlerLogin(w http.ResponseWriter, r *http.Request) {
 	}{
 		Token: tokenString,
 		User:  domain.DatabaseUserToUser(user),
+	}
+
+	// Respond with the token and user data
+	util.RespondWithJSON(w, http.StatusOK, response)
+
+}
+
+// HandlerLogin handles the request to login to an existing user. A cookie containing the JWT will be returned.
+func HandlerStudentLogin(w http.ResponseWriter, r *http.Request) {
+
+	apiCfg := r.Context().Value(contextKeys.ConfigKey).(*config.ApiConfig)
+
+	// Decode the JSON request body into parameters struct
+	type parameters struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+	defer r.Body.Close()
+
+	if params.Username == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "Username is required")
+		return
+	} else if params.Password == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "Password is required")
+		return
+	}
+
+	passwordHash, err := apiCfg.DB.GetStudentHash(r.Context(), params.Username)
+
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving passwordHash: %v", err))
+		return
+	}
+
+	// Check if the password matches the hashed password in the database
+	if !util.CheckPasswordHash(params.Password, passwordHash) {
+		util.RespondWithError(w, http.StatusBadRequest, "Wrong password")
+		return
+	}
+
+	// Query for student
+	student, err := apiCfg.DB.GetStudentByUsername(r.Context(), params.Username)
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving student info: %v", err))
+		return
+	}
+
+	// Define the standard claims
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "github.com/yizhong187/EduMind-backend",
+		Subject:   student.StudentID.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(30 * 24 * time.Hour)), // 30 days
+	}
+
+	// Create a new token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with the secret key
+	tokenString, err := token.SignedString([]byte(apiCfg.SecretKey))
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Could not login")
+		http.Error(w, "could not login", http.StatusInternalServerError)
+		return
+	}
+
+	// Create the response object containing the token and user data
+	response := struct {
+		Token   string         `json:"token"`
+		Student domain.Student `json:"student"`
+	}{
+		Token:   tokenString,
+		Student: domain.DatabaseStudentToStudent(student),
+	}
+
+	// Respond with the token and user data
+	util.RespondWithJSON(w, http.StatusOK, response)
+
+}
+
+// HandlerLogin handles the request to login to an existing user. A cookie containing the JWT will be returned.
+func HandlerTutorLogin(w http.ResponseWriter, r *http.Request) {
+
+	apiCfg := r.Context().Value(contextKeys.ConfigKey).(*config.ApiConfig)
+
+	// Decode the JSON request body into parameters struct
+	type parameters struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+	defer r.Body.Close()
+
+	if params.Username == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "Username is required")
+		return
+	} else if params.Password == "" {
+		util.RespondWithError(w, http.StatusBadRequest, "Password is required")
+		return
+	}
+
+	passwordHash, err := apiCfg.DB.GetTutorHash(r.Context(), params.Username)
+
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving passwordHash: %v", err))
+		return
+	}
+
+	// Check if the password matches the hashed password in the database
+	if !util.CheckPasswordHash(params.Password, passwordHash) {
+		util.RespondWithError(w, http.StatusBadRequest, "Wrong password")
+		return
+	}
+
+	// Query for tutor
+	tutor, err := apiCfg.DB.GetTutorByUsername(r.Context(), params.Username)
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("Error retrieving tutor info: %v", err))
+		return
+	}
+
+	// Define the standard claims
+	claims := &jwt.RegisteredClaims{
+		Issuer:    "github.com/yizhong187/EduMind-backend",
+		Subject:   tutor.TutorID.String(),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(30 * 24 * time.Hour)), // 30 days
+	}
+
+	// Create a new token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Sign the token with the secret key
+	tokenString, err := token.SignedString([]byte(apiCfg.SecretKey))
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Could not login")
+		http.Error(w, "could not login", http.StatusInternalServerError)
+		return
+	}
+
+	tutorSubjects, err := apiCfg.DB.GetTutorSubjects(r.Context(), tutor.TutorID)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't get tutor subjects")
+		return
+	}
+
+	// Create the response object containing the token and user data
+	response := struct {
+		Token string       `json:"token"`
+		Tutor domain.Tutor `json:"tutor"`
+	}{
+		Token: tokenString,
+		Tutor: domain.DatabaseTutorToTutor(tutor, tutorSubjects),
 	}
 
 	// Respond with the token and user data
