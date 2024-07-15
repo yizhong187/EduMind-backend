6d8074786a1a9796603c06cef18349fa5fb29752
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:08:32 +0800

    mounted new endpoints

--- routers/tutor_router.go
+++ routers/tutor_router.go
@@ -14,11 +14,16 @@ func TutorRouter(apiCfg *config.ApiConfig) *chi.Mux {
 		r.Get("/healthz", handlers.HandlerReadiness)
 		r.Get("/err", handlers.HandlerError)
 
+		r.Post("/register", handlers.HandlerTutorRegistration)
+		r.Post("/login", handlers.HandlerTutorLogin)
+
 		rAuthenticated := chi.NewRouter()
 		rAuthenticated.Use(middlewares.MiddlewareTutorAuth)
 		rAuthenticated.Get("/profile", handlers.HandlerGetTutorProfile)
 		rAuthenticated.Put("/profile", handlers.HandlerUpdateTutorProfile)
-		rAuthenticated.Get("/studentProfile", handlers.HandlerTutorGetStudentProfile)
+		rAuthenticated.Get("/student-profile", handlers.HandlerTutorGetStudentProfile)
+		rAuthenticated.Get("/pending-questions", handlers.HandlerGetAvailableQuestions)
+		rAuthenticated.Put("/accept", handlers.HandlerConfigNewChat)
 
 		r.Mount("/", rAuthenticated)
 	})
