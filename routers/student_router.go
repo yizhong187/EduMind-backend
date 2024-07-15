fff34262096b741f81a8cdccca09d7530a859e3f
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:08:40 +0800

    mounted new endpoints

--- routers/student_router.go
+++ routers/student_router.go
@@ -15,11 +15,13 @@ func StudentRouter(apiCfg *config.ApiConfig) *chi.Mux {
 		r.Get("/err", handlers.HandlerError)
 
 		r.Post("/register", (handlers.HandlerStudentRegistration))
+		r.Post("/login", (handlers.HandlerStudentLogin))
 
 		rAuthenticated := chi.NewRouter()
 		rAuthenticated.Use(middlewares.MiddlewareStudentAuth)
 		rAuthenticated.Get("/profile", handlers.HandlerGetStudentProfile)
 		rAuthenticated.Put("/profile", handlers.HandlerUpdateStudentProfile)
+		rAuthenticated.Post("/new-question", handlers.HandlerNewQuestion)
 
 		r.Mount("/", rAuthenticated)
 
