fcc1f09e5d2bc9cf7249eda017a4ece7dfda0206
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:49:12 +0800

    removed 2 deprecated endpoints

--- main.go
+++ main.go
@@ -86,8 +86,9 @@ func main() {
 	})
 	v1Router.Get("/healthz", handlers.HandlerReadiness)
 	v1Router.Get("/error", handlers.HandlerError)
-	v1Router.Post("/login", handlers.HandlerLogin)
-	v1Router.Get("/logout", handlers.HandlerLogout)
+	// DEPRECATING
+	// v1Router.Post("/login", handlers.HandlerLogin)
+	// v1Router.Get("/logout", handlers.HandlerLogout)
 
 	v1Router.Mount("/students", routers.StudentRouter(&apiCfg))
 	v1Router.Mount("/tutors", routers.TutorRouter(&apiCfg))
