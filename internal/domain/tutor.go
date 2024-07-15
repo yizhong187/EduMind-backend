8ae964f308e630c0cdcca218f3887ab929026278
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:11:18 +0800

    updated to reflect in database schema

--- internal/domain/tutor.go
+++ internal/domain/tutor.go
@@ -7,6 +7,11 @@ import (
 	"github.com/yizhong187/EduMind-backend/internal/database"
 )
 
+type Subject struct {
+	Name string `json:"subject"`
+	Yoe  int32  `json:"yoe"`
+}
+
 type Tutor struct {
 	TutorID     uuid.UUID `json:"tutor_id"`
 	Username    string    `json:"username"`
@@ -14,14 +19,21 @@ type Tutor struct {
 	CreatedAt   time.Time `json:"created_at"`
 	Name        string    `json:"name"`
 	Valid       bool      `json:"valid"`
-	YOE         int32     `json:"yoe"`
-	Subject     string    `json:"subject"`
+	Subjects    []Subject `json:"subjects"`
 	Verified    bool      `json:"verified"`
 	Rating      float64   `json:"rating"`
 	RatingCount int32     `json:"rating_count"`
 }
 
-func DatabaseTutorToTutor(tutor database.Tutor) Tutor {
+func DatabaseTutorToTutor(tutor database.Tutor, subjects []database.GetTutorSubjectsRow) Tutor {
+	subjectsList := make([]Subject, len(subjects))
+	for i, subject := range subjects {
+		subjectsList[i] = Subject{
+			Name: subject.Subject,
+			Yoe:  subject.Yoe,
+		}
+	}
+
 	return Tutor{
 		TutorID:     tutor.TutorID,
 		Username:    tutor.Username,
@@ -29,10 +41,9 @@ func DatabaseTutorToTutor(tutor database.Tutor) Tutor {
 		CreatedAt:   tutor.CreatedAt,
 		Name:        tutor.Name,
 		Valid:       tutor.Valid,
-		YOE:         tutor.Yoe,
-		Subject:     tutor.Subject,
 		Verified:    tutor.Verified,
 		Rating:      tutor.Rating.Float64,
 		RatingCount: tutor.RatingCount,
+		Subjects:    subjectsList,
 	}
 }
