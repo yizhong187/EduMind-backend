c0c7e6f28b5c2a5084385444404961aca53d25af
yizhong187 <yizhong187@gmail.com>
2024-07-15 14:08:00 +0800

    added new sql queries for tutors and edited tutor table insertion based on new schema

--- sql/queries/tutors.sql
+++ sql/queries/tutors.sql
@@ -1,7 +1,33 @@
 -- name: CreateNewTutor :one
-INSERT INTO tutors (tutor_id, username, email, created_at, name, valid, hashed_password, yoe, subject, verified, rating, rating_count)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
+INSERT INTO tutors (tutor_id, username, email, created_at, name, valid, hashed_password, verified, rating, rating_count)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
 RETURNING *;
+
+-- name: AddTutorSubject :one
+INSERT INTO tutor_subjects (tutor_id, subject_id, yoe)
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: GetTutorSubjects :many
+SELECT 
+    s.name AS subject,
+    ts.yoe
+FROM 
+    tutor_subjects ts
+JOIN 
+    subjects s ON ts.subject_id = s.subject_id
+WHERE 
+    ts.tutor_id = $1;
+
+-- name: GetTutorSubjectIDs :many
+SELECT subject_id
+FROM tutor_subjects
+WHERE tutor_id = $1;
+
+-- name: GetSubjectIDByName :one
+SELECT subject_id
+FROM subjects
+WHERE name = $1;
 
 -- name: GetTutorById :one
 SELECT * FROM tutors WHERE tutor_id = $1;
