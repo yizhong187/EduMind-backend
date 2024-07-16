79e9c2267fd9afdfecba3a720f6d33f21b5df673
yizhong187 <yizhong187@gmail.com>
2024-07-16 18:56:46 +0800

    changed tutorID and photoURL from nullable to a go pointer for better json parsing

--- internal/domain/chat.go
+++ internal/domain/chat.go
@@ -9,27 +9,41 @@ import (
 )
 
 type Chat struct {
-	ChatID    int32          `json:"chat_id"`
-	StudentID uuid.UUID      `json:"student_id"`
-	TutorID   uuid.NullUUID  `json:"tutor_id"`
-	CreatedAt time.Time      `json:"created_at"`
-	SubjectID int32          `json:"subject"`
-	Topics    []int32        `json:"topic"`
-	Header    string         `json:"header"`
-	PhotoURL  sql.NullString `json:"photo_url"`
-	Completed bool           `json:"completed"`
+	ChatID    int32      `json:"chat_id"`
+	StudentID uuid.UUID  `json:"student_id"`
+	TutorID   *uuid.UUID `json:"tutor_id"`
+	CreatedAt time.Time  `json:"created_at"`
+	SubjectID int32      `json:"subject"`
+	Topics    []int32    `json:"topic"`
+	Header    string     `json:"header"`
+	PhotoURL  *string    `json:"photo_url"`
+	Completed bool       `json:"completed"`
 }
 
 func DatabaseChatToChat(chat database.Chat, topics []int32) Chat {
 	return Chat{
 		ChatID:    chat.ChatID,
 		StudentID: chat.StudentID,
-		TutorID:   chat.TutorID,
+		TutorID:   nullUUIDToUUID(chat.TutorID),
 		CreatedAt: chat.CreatedAt,
 		SubjectID: chat.SubjectID,
 		Topics:    topics,
 		Header:    chat.Header,
-		PhotoURL:  chat.PhotoUrl,
+		PhotoURL:  nullStringToString(chat.PhotoUrl),
 		Completed: chat.Completed,
 	}
+}
+
+func nullUUIDToUUID(nu uuid.NullUUID) *uuid.UUID {
+	if nu.Valid {
+		return &nu.UUID
+	}
+	return nil
+}
+
+func nullStringToString(ns sql.NullString) *string {
+	if ns.Valid {
+		return &ns.String
+	}
+	return nil
 }
