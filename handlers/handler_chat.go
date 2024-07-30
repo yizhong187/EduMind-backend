57e6c2f7b5b47b71e79e5c8819a733acda5594cd
yizhong187 <yizhong187@gmail.com>
2024-07-30 16:10:21 +0800

    added update rating handler

--- handlers/handler_chat.go
+++ handlers/handler_chat.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -169,4 +170,122 @@ func HandlerCompleteChat(w http.ResponseWriter, r *http.Request) {
 	}
 
 	util.RespondWithJSON(w, http.StatusOK, "Chat marked as complete.")
+}
+
+func HandlerUpdateRating(w http.ResponseWriter, r *http.Request) {
+	apiCfg, ok := r.Context().Value(contextKeys.ConfigKey).(*config.ApiConfig)
+	if !ok || apiCfg == nil {
+		fmt.Println("ApiConfig not found.")
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	chatIDString := chi.URLParam(r, "chatID")
+	chatID, err := strconv.ParseInt(chatIDString, 10, 32)
+	if err != nil {
+		fmt.Println(err)
+		util.RespondWithError(w, http.StatusInternalServerError, "Invalid chat ID")
+		return
+	}
+
+	chat, err := apiCfg.DB.GetChatById(r.Context(), int32(chatID))
+	if err != nil {
+		fmt.Println("Could not retrieve chat: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	if !chat.Completed {
+		fmt.Println("Chat not completed.")
+		util.RespondWithError(w, http.StatusBadRequest, "Chat not completed.")
+		return
+	}
+
+	if chat.Rating.Valid {
+		fmt.Println("Chat has previously been rated.")
+		util.RespondWithError(w, http.StatusConflict, "Chat has previously been rated.")
+		return
+	}
+
+	type parameters struct {
+		Rating int32 `json:"rating"`
+	}
+
+	params := parameters{}
+	err = json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		fmt.Println("Couldn't decode parameters", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+	defer r.Body.Close()
+
+	if params.Rating < 1 || params.Rating > 5 {
+		fmt.Println("Invalid rating: ", params.Rating)
+		util.RespondWithError(w, http.StatusBadRequest, "Invalid rating or missing rating parameter.")
+		return
+	}
+
+	tx, err := apiCfg.DBConn.BeginTx(r.Context(), nil)
+	if err != nil {
+		fmt.Println("Couldn't start transaction: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	defer tx.Rollback()
+
+	queries := apiCfg.DB.WithTx(tx)
+
+	chat, err = queries.UpdateChatRating(r.Context(), database.UpdateChatRatingParams{
+		ChatID: int32(chatID),
+		Rating: sql.NullInt32{
+			Int32: params.Rating,
+			Valid: true,
+		},
+	})
+	if err != nil {
+		fmt.Println("Could not update chat rating: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	tutor, err := queries.GetTutorById(r.Context(), chat.TutorID.UUID)
+	if err != nil {
+		fmt.Println("Could not retrieve tutor info: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	var newRating sql.NullFloat64
+	if tutor.RatingCount == 0 {
+		newRating = sql.NullFloat64{
+			Float64: float64(params.Rating),
+			Valid:   true,
+		}
+	} else {
+		newRating = sql.NullFloat64{
+			Float64: (tutor.Rating.Float64*float64(tutor.RatingCount) + float64(params.Rating)) / (tutor.Rating.Float64 + 1),
+			Valid:   true,
+		}
+	}
+
+	err = queries.UpdateTutorRatings(r.Context(), database.UpdateTutorRatingsParams{
+		TutorID:     tutor.TutorID,
+		Rating:      newRating,
+		RatingCount: tutor.RatingCount + 1,
+	})
+	if err != nil {
+		fmt.Println("Could not update tutor rating: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	if err := tx.Commit(); err != nil {
+		fmt.Println("Couldn't commit transaction: ", err)
+		util.RespondWithInternalServerError(w)
+		return
+	}
+
+	util.RespondWithJSON(w, http.StatusOK, "Successfully rated chat and tutor.")
 }
